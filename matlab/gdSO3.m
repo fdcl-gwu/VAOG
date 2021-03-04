64d1cc8440794c2a9962ba7ab787c6d37427d613
tylee <tylee.fdcl@gmail.com>
2021-03-03 21:07:22 -0500

    DEL resived

--- matlab/gdSO3.m
+++ matlab/gdSO3.m
@@ -6,9 +6,9 @@ global A
 A=rand(3,3);%diag([5, 3, 1])*expm(pi*hat([1 1 1])/sqrt(3));
 [U S V]=psvd(A);
 R_opt = U*V';
-J_opt = trace((A-R_opt)'*(A-R_opt));
+J_opt = obj(R_opt);
 
-p=2;
+p=3;
 C=1;
 J=eye(3);
 
@@ -17,13 +17,14 @@ R0=eye(3);
 X0=reshape(R0,9,1);
 tspan=[1 100];
 
-options = odeset('RelTol',1e-8,'AbsTol',1e-10);
-[t X]=ode45(@eom_GD, tspan, X0, options);
-N=length(t);
+options = odeset('RelTol',1e-8,'AbsTol',1e-8);
+sol = ode45(@eom_GD, tspan, X0, options);
+t=sol.x;X=sol.y';disp(sol.stats);
 
+N=length(t);
 for k=1:N
     R_GD(:,:,k) = reshape(X(k,:),3,3);
-    J_GD(k) = trace((A-R_GD(:,:,k))'*(A-R_GD(:,:,k)))-J_opt;
+    J_GD(k) = obj(R_GD(:,:,k))-J_opt;
 end
 
 t_GD=t;
@@ -33,13 +34,15 @@ R0=eye(3);
 W0=zeros(3,1);
 X0=[reshape(R0,9,1); W0];
 
-[t X]=ode45(@eom_AGD, tspan, X0, options);
+sol=ode45(@eom_AGD, tspan, X0, options);
+t=sol.x;X=sol.y';disp(sol.stats);
+
 N=length(t);
 W=X(:,10:12)';
 
 for k=1:N
     R_AGD(:,:,k) = reshape(X(k,1:9),3,3);
-    J_AGD(k) = trace((A-R_AGD(:,:,k))'*(A-R_AGD(:,:,k)))-J_opt;
+    J_AGD(k) = obj(R_AGD(:,:,k))-J_opt;
     Pi_AGD(:,k) = t(k)^(p+1)/p*J*W(:,k);
 end
 t_AGD = t;
@@ -50,18 +53,21 @@ R0=eye(3);
 Pi0=zeros(3,1);
 X0=[reshape(R0,9,1); W0];
 
-[t X]=ode45(@eom_AGD_Pi, tspan, X0, options);
+sol=ode45(@eom_AGD_Pi, tspan, X0, options);
+t=sol.x;X=sol.y';disp(sol.stats);
+
 N=length(t);
 Pi=X(:,10:12)';
 
 for k=1:N
     R_AGD_Pi(:,:,k) = reshape(X(k,1:9),3,3);
-    J_AGD_Pi(k) = trace((A-R_AGD_Pi(:,:,k))'*(A-R_AGD_Pi(:,:,k)))-J_opt;
+    J_AGD_Pi(k) = obj(R_AGD_Pi(:,:,k))-J_opt;
 end
 t_AGD_Pi = t;
 
 
 %% Post Process
+
 plot(t_GD ,J_GD, 'r', t_AGD, J_AGD,'b', t_AGD_Pi, J_AGD_Pi,'g');
 set(gca,'xscale','log','yscale','log');
 
@@ -72,21 +78,36 @@ evalin('base',['load ' filename]);
 
 end
 
+function J = obj(R)
+global A
+
+tmp=R-A;
+J = tmp(:)'*tmp(:)/2;
+
+end
+
+function M = grad(R)
+global A
+
+M=vee(R'*A-A'*R);%+randn(3,1)*1e-3;
+
+end
+
 
 function X_dot = eom_GD(t,X)
-global A
 R=reshape(X,3,3);
-M = vee(R'*A-A'*R);
+M = grad(R);
+
 R_dot = R*hat(M);
 X_dot = reshape(R_dot,9,1);
 end
 
 function X_dot = eom_AGD(t,X)
-global A p J C
+global p J C
 R=reshape(X(1:9),3,3);
 W=X(10:12);
 
-M = vee(R'*A-A'*R);
+M = grad(R);
 R_dot = R*hat(W);
 W_dot = J\( -(p+1)/t*J*W - hat(W)*J*W + C*p^2*t^(p-2)*M);
 
@@ -94,11 +115,11 @@ X_dot = [reshape(R_dot,9,1); W_dot];
 end
 
 function X_dot = eom_AGD_Pi(t,X)
-global A p J C
+global p J C
 R=reshape(X(1:9),3,3);
 Pi=X(10:12);
 
-M = vee(R'*A-A'*R);
+M = grad(R);
 W = J\(p/t^(p+1)*Pi);
 
 R_dot = R*hat(W);
